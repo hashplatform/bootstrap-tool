2c55c0c5381beb05f451016d6f8f0304829414e5
Jack Coble <jackkdev@protonmail.com>
2019-04-14 18:49:55 +0100

    Fix spelling errors

--- cmd/bootstrap-tool/main_test.go
+++ cmd/bootstrap-tool/main_test.go
@@ -4,22 +4,22 @@ import "testing"
 
 func TestLoadConfigFile(t *testing.T) {
 	config := struct {
-		Coin string
-		Directory string
+		Coin        string
+		Directory   string
 		Destination string
-	} {
+	}{
 		"bitcoin",
 		"../../testData",
 		"../../testingArchives",
 	}
 
 	if config.Coin == "" {
-		t.Errorf("Coin paramter is empty, got: %s, want: %s", config.Coin, "bitcoin")
+		t.Errorf("Coin parameter is empty, got: %s, want: %s", config.Coin, "bitcoin")
 	}
 	if config.Directory == "" {
-		t.Errorf("Directory paramter is empty, got: %s, want: %s", config.Directory, "../../testData")
+		t.Errorf("Directory parameter is empty, got: %s, want: %s", config.Directory, "../../testData")
 	}
 	if config.Destination == "" {
-		t.Errorf("Directory paramter is empty, got: %s, want: %s", config.Destination, "../../testingArchives")
+		t.Errorf("Directory parameter is empty, got: %s, want: %s", config.Destination, "../../testingArchives")
 	}
 }
